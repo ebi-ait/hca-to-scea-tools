44f50f29b0fa189162083ab029ed5ba3892787aa
Ami Day <ami@ebi.ac.uk>
2021-10-26 13:06:29 +0100

    Added directory with example output files for SOP

--- hca2scea-backend/parse_xml.py
+++
@@ -1,5 +0,0 @@
-
-def get_experiment_pkg_objects(xml_content):
-    """Get experiment package objects from xml."""
-    for experiment_package in xml_content.findall('EXPERIMENT_PACKAGE'):
-        yield experiment_package
