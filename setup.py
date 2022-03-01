c3cd7f5462386b8f9c4e667178b2fd313557dbf7
Ami Day <ami@ebi.ac.uk>
2022-03-01 10:46:12 +0000

    Added requirements.txt Added setup file

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
-# The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
 README = (HERE / "README.md").read_text()
+install_requires = [line.rstrip() for line in (HERE / "requirements.txt").read_text().splitlines()]
 
-# This call to setup() does all the work
 setup(
     name="hca-to-scea",
     version="1.0.0",
-    description="A tool to convert an HCA metadata spreadsheet to SCEA MAGE-TAB metadata files.",
+    description="A tool to assist in the automatic conversion of hca metadata to scea metadata MAGE-TAB files.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/ebi-ait/hca-to-scea-tools",
     author="Ami Day, Javier Ferrer, Yusra Haider, Alegria Aclan",
     author_email="ami@ebi.ac.uk, javier.f.g@um.es, yhaider@ebi.ac.uk, aaclan@ebi.ac.uk",
-    license="MIT",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
     ],
-    packages=["hca-to-scea"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=install_requires,
     include_package_data=True,
-    install_requires=["glob","requests","numpy","pandas","elementpath","contextlib2"],
     entry_points={
         "console_scripts": [
-            "hca-to-scea=hca2scea.__main__:main",
+            "hca-to-scea=hca_to_scea-tools.hca2scea:main",
         ]
     },
 )
