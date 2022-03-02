dd70389bd8efa28ee102ab767fbc586d9935baec
Ami Day <ami@ebi.ac.uk>
2022-03-02 10:03:19 +0000

    Made changes suggested by Jacob. Setup.py file: changed version and author name order. Manifest: removed requirements.txt file.

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ install_requires = [line.rstrip() for line in (HERE / "requirements.txt").read_t
 
 setup(
     name="hca-to-scea",
-    version="1.0.0",
+    version="v0.1.0",
     description="A tool to assist in the automatic conversion of hca metadata to scea metadata MAGE-TAB files.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/ebi-ait/hca-to-scea-tools",
-    author="Ami Day, Javier Ferrer, Yusra Haider, Alegria Aclan",
-    author_email="ami@ebi.ac.uk, javier.f.g@um.es, yhaider@ebi.ac.uk, aaclan@ebi.ac.uk",
+    author="Ami Day, Yusra Haider, Alegria Aclan, Javier Ferrer",
+    author_email="ami@ebi.ac.uk, yhaider@ebi.ac.uk, aaclan@ebi.ac.uk, javier.f.g@um.es",
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: Apache Software License",
