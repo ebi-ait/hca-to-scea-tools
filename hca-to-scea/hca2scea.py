80de485536c782654e81474e5bc051d7a07eb05f
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2025-06-26 19:01:42 +0300

    Added option to skip empty secondary accessions

--- hca-to-scea/hca2scea.py
+++ hca-to-scea/hca2scea.py
@@ -24,6 +24,8 @@ def get_secondary_accessions(xlsx_dict, args):
     secondary_accessions.append(args.project_uuid)
     keys = ["project.geo_series_accessions","project.insdc_project_accessions","project.insdc_study_accessions","project.biostudies_accessions"]
     for key in keys:
+        if key not in xlsx_dict["project"]:
+            continue
         items = xlsx_dict["project"][key].fillna('')
         items = [item for item in items if item != '']
         if items:
