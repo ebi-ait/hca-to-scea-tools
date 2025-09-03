8fcbf9446c3dc1ce19d85e65aa076ab13c21a15d
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2025-09-03 08:59:22 +0100

    Fix merge with overlap cell_lines.py

--- hca-to-scea/helpers/cell_lines.py
+++ hca-to-scea/helpers/cell_lines.py
@@ -11,6 +11,15 @@ def get_specimen(xlsx_dict, type):
 
     return specimen_df
 
+def fix_overlap(df):
+    for col_x in df.columns:
+        if col_x.endswith("_x"):
+            fixed_col = col_x[:-2]
+            col_y = fixed_col + "_y"
+            df[fixed_col] = df[col_x].combine_first(df[col_y])
+            df = df.drop(columns=[col_x, col_y], axis=1)
+    return df
+
 def merge_cell_lines(xlsx_dict, merged_df, experimental_design):
 
     if experimental_design == "cell_line_only":
@@ -20,6 +29,7 @@ def merge_cell_lines(xlsx_dict, merged_df, experimental_design):
             how="outer",
             on="cell_line.biomaterial_core.biomaterial_id"
         )
+        merged_df = fix_overlap(merged_df)
 
         specimen_df = get_specimen(xlsx_dict, type="cell_line")
         merged_df = merged_df.merge(
@@ -27,6 +37,7 @@ def merge_cell_lines(xlsx_dict, merged_df, experimental_design):
             how="outer",
             on="cell_line.biomaterial_core.biomaterial_id"
             )
+        merged_df = fix_overlap(merged_df)
 
     elif experimental_design == "organoid_only":
 
@@ -35,6 +46,7 @@ def merge_cell_lines(xlsx_dict, merged_df, experimental_design):
             how="outer",
             on="organoid.biomaterial_core.biomaterial_id"
         )
+        merged_df = fix_overlap(merged_df)
 
         specimen_df = get_specimen(xlsx_dict, type="organoid")
         merged_df = merged_df.merge(
@@ -42,6 +54,7 @@ def merge_cell_lines(xlsx_dict, merged_df, experimental_design):
             how="outer",
             on="cell_line.biomaterial_core.biomaterial_id"
             )
+        merged_df = fix_overlap(merged_df)
 
     elif experimental_design == "organoid":
 
@@ -50,12 +63,14 @@ def merge_cell_lines(xlsx_dict, merged_df, experimental_design):
             how="outer",
             on="organoid.biomaterial_core.biomaterial_id"
         )
+        merged_df = fix_overlap(merged_df)
 
         merged_df = xlsx_dict['cell_line'].merge(
             merged_df,
             how="outer",
             on="cell_line.biomaterial_core.biomaterial_id"
         )
+        merged_df = fix_overlap(merged_df)
 
         specimen_df = get_specimen(xlsx_dict, type="cell_line")
         merged_df = merged_df.merge(
@@ -63,5 +78,6 @@ def merge_cell_lines(xlsx_dict, merged_df, experimental_design):
             how="outer",
             on="cell_line.biomaterial_core.biomaterial_id"
             )
+        merged_df = fix_overlap(merged_df)
 
     return merged_df
