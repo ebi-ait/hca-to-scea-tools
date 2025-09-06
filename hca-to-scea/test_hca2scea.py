2cc67ac6fd15fe70a9d627078faa126823c10e7b
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2025-09-06 23:01:55 +0100

    Add reset_index to sorted dataframes in assert_dataframes_contents_equal

--- hca-to-scea/test_hca2scea.py
+++ hca-to-scea/test_hca2scea.py
@@ -91,11 +91,11 @@ class CharacteristicTest(unittest.TestCase):
         golden_contents, output_contents = self.get_dfs_even(golden_contents, output_contents, extra_cols)
 
         if 'Source Name' in golden_contents.columns:
-            golden_contents = golden_contents.sort_values(by=['Source Name'])
-            output_contents = output_contents.sort_values(by=['Source Name'])
+            golden_contents = golden_contents.sort_values(by=['Source Name']).reset_index(drop=True)
+            output_contents = output_contents.sort_values(by=['Source Name']).reset_index(drop=True)
         elif 'name' in golden_contents.columns:
-            golden_contents = golden_contents.sort_values(by=['name'])
-            output_contents = output_contents.sort_values(by=['name'])
+            golden_contents = golden_contents.sort_values(by=['name']).reset_index(drop=True)
+            output_contents = output_contents.sort_values(by=['name']).reset_index(drop=True)
         diff = golden_contents.compare(output_contents, result_names=('expected', 'actual'))
         
         diff = diff.melt(value_name='diff_value')
