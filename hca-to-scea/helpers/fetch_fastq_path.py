8ee9c4f71d24a624a6ce13dbd296be03bd08f5fe
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2025-09-07 14:42:18 +0100

    Fix get_fastq_path_from_ena test with single argument after eded1eb994f0efcda5e1fd4f20085f9a8f82990d

--- hca-to-scea/helpers/fetch_fastq_path.py
+++ hca-to-scea/helpers/fetch_fastq_path.py
@@ -171,6 +171,7 @@ def get_fastq_path_from_ena(run_accessions):
             return {}
         paths_fastq[accession] = {'files': []}
         file_list = str(fastq_results[url_col].values[0]).split(';')
+        # TODO skip per file not whole project
         if len(file_list) < 2:
             return {}
         for file_path in file_list:
--- hca-to-scea/test_fetch_fastq.py
+++ hca-to-scea/test_fetch_fastq.py
@@ -68,7 +68,7 @@ class TestSraUtils(unittest.TestCase):
         })
         mock_read_csv.return_value = fake_df
 
-        result = fetch_fastq_path.get_fastq_path_from_ena("PRJNA123", ["SRR111"])
+        result = fetch_fastq_path.get_fastq_path_from_ena(["SRR111"])
 
         self.assertIn("SRR111", result)
         self.assertEqual(len(result["SRR111"]["files"]), 2)
